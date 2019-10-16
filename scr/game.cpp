5135b4efa699c5d6132cfb662331a21269c20e98
10maurycy10 <10maurycy10@users.noreply.github.com>
2019-10-16 10:09:34 -0700

    fix the map and fix it all( incuding colisons and map rendering )

--- scr/game.cpp
+++ scr/game.cpp
@@ -3,37 +3,37 @@
 
 
 
-#define DEBUG
+#define DEBUGCOMP
 
-#Ifdef DEBUG
+#ifdef DEBUGCOP
 void debug(char* a) {
-  ;
+  printf("%s\n",a);
 }
 #else
 void debug(char* a) {;}
-
+#endif
 
 
 
 
 
 enum boutons {BOSS,EXIT,INV,UP,DOWN,RIGHT,LEFT,RESTART,DROP,DEBUG}; //the controls
-constexpr int cont[10] = {13,113,105,104,106,108,107,114,100,99}; //the keys for the controls
-const char* items[2] = {"(empty)","test   "};//the item names
+constexpr int gCont[10] = {13,113,105,104,106,108,107,114,100,99}; //the keys for the controls
+const char* gItems[2] = {"(empty)","test   "};//the item names
 
 #define INVT 10 //the invetory size
 int inv[10] = {0,0,0,0,0,0,0,0,0,0};//the invetry itslef
 
-const char* verson = "0.6";
+const char* gVerson = "0.7";
 
-int playerx = 2;//player posision
-int playery = 1;
+int gPlayerx = 2;//player posision
+int gPlayery = 1;
 int gRoom = 0;
 
 #define MAPY 29 //map size
 #define MAPX 26
 
-char rendermap[MAPY][MAPX+1] = { //map + fog of war
+char rendermap[MAPY][MAPX+2] = { //map + fog of war
   "..........................",
   "..........................",
   "..........................",
@@ -66,13 +66,14 @@ char rendermap[MAPY][MAPX+1] = { //map + fog of war
 };
 
 
-char map[MAPY][MAPX+1] = {//the map
+
+char map[MAPY][MAPX+2] = {//the map
   "XXXXXXX...XXXXXXXX........",
-  "..........X......X...XXXXX",
-  "..........X......X...X...X",
-  "..........+......XXXXX...X",
-  "..........X......+###+...X",
-  "..........X......XXXXX+XX.",
+  "X.....X...X......X...XXXXX",
+  "X.....XXXXX......X...X...X",
+  "X.....+###+......XXXXX...X",
+  "X.....XXXXX......+###+...X",
+  "XXXXXXX...X......XXXXX+XX.",
   "..........XXXXX+XX....#...",
   "......................#...",
   "..........................",
@@ -99,7 +100,7 @@ char map[MAPY][MAPX+1] = {//the map
   };
 
   struct obj { //entaty data
-  int id; /* 0: test/null  1 - 101: items 101 - 201: mosters/npcs*/
+  int id; /* 0: test/null  1 - 101: gItems 101 - 201: mosters/npcs*/
   int x;
   int y;
 };
@@ -107,7 +108,7 @@ char map[MAPY][MAPX+1] = {//the map
 struct obj* mapobj[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //pointers for the entitys
 #define ROOM 1 //room count
 int roomdata[ROOM][4] = { //the room data
-  {0,0,MAPX,MAPY},  //test room
+  {0,0,MAPX,MAPY-1},  //test room
   //{0 ,0 ,20,5},
   //{0 ,7 ,20,13},
   //{0,15 ,20,20}
@@ -157,8 +158,8 @@ void clearmap() {//reset the fog of doooooooooooooooooooomm
 int getRoomId(int x,int y) {
 
   for (int i = 0;i<ROOM;i++) {//for all rooms
-  if ((roomdata[i][0]<=(playerx)) && (roomdata[i][1]<=(playery))) {//if is in romm:
-    if ((roomdata[i][2]>=playerx) && (roomdata[i][3])>=playery) {
+  if ((roomdata[i][0]<=(gPlayerx)) && (roomdata[i][1]<=(gPlayery))) {//if is in romm:
+    if ((roomdata[i][2]>=gPlayerx) && (roomdata[i][3])>=gPlayery) {
         //mvprintw(0,0,"%d",i+1);
         return i+1; //return the roomid
       }
@@ -176,27 +177,27 @@ void msg(const char* a) { //print a msg
 
 void renderline(char*,int); //redraw a line
 
-void cleanln(int x) { //redray a line x: on screan pos
-  move(x,0);
-  if (x > 0) {
-    renderline(rendermap[x-1],x);
+void cleanln(int y) { //redray a line x: on screan pos
+  move(y,0);
+  if (y > 0) {
+    renderline(rendermap[y-1],y);
   }
 
-  for (int y=0;y<OBJ;y++) { //render entetys
-    if (mapobj[y] == NULL) {;}else{
-      if ((*(mapobj[y])).y+1 == x) {
-        if (getRoomId((*(mapobj[y])).x,(*(mapobj[y])).y)==gRoom){
-          switch ((*(mapobj[y])).id) {
-            case 1:mvaddch(x,(*(mapobj[y])).x,73);break;
-            case 0:mvaddch(x,(*(mapobj[y])).x,109);break;
+  for (int ypos=0;ypos<OBJ;ypos++) { //render entetys
+    if (mapobj[ypos] == NULL) {;}else{
+      if ((*(mapobj[ypos])).y+1 == ypos) {
+        if (getRoomId((*(mapobj[ypos])).x,(*(mapobj[ypos])).y)==gRoom){
+          switch ((*(mapobj[ypos])).id) {
+            case 1:mvaddch(ypos,(*(mapobj[ypos])).x,73);break;
+            case 0:mvaddch(ypos,(*(mapobj[ypos])).x,109);break;
           }
         }
       }
     }
   }
 
-  if (x-1==playery) {//draw player
-    mvaddch(playery+1,playerx,'@');
+  if (y-1==gPlayery) {//draw player
+    mvaddch(y,gPlayerx,'@');
   }
 }
 
@@ -269,8 +270,8 @@ void restart(bool a) { //reset it all a:if tho reset inv
   movep(3,2);
   genaratemap();
   clearmap();
+  fillroom(getRoomId(gPlayerx,gPlayery));
   render();
-  fillroom(getRoomId(playerx,playery));
 }
 
 void movep(int x,int y) { //move the player
@@ -285,14 +286,17 @@ void movep(int x,int y) { //move the player
     }
   }
 
-  int oy = playery;
-  if (map[y][x]=='X') {cleanln(y+1);;return;} else{
-    playerx = x;
-    playery = y;
+  int oy = gPlayery;
+  if (map[y][x]=='X') {
+    cleanln(y+1);
+    return;
+  } else {
+    gPlayerx = x;
+    gPlayery = y;
     cleanln(y+1);
   }
   fillroom(gRoom);
-  gRoom = getRoomId(playerx,playery);
+  gRoom = getRoomId(gPlayerx,gPlayery);
   fillroom(gRoom);
   if (map[y][x]=='%') {
     msg("next leaval");
@@ -310,7 +314,7 @@ void inventory() {
   msg("you have : -more-");
   for (int y = 0;y<INVT;y++) {
     move(y+1,0);
-    printw("-  %d : %s  -",y,items[inv[y]]);
+    printw("-  %d : %s  -",y,gItems[inv[y]]);
   }
   getch();
   for (int y = 0;y<INVT;y++) {
@@ -335,8 +339,8 @@ void drop() {
   }
 
   obj* o = (obj*)malloc(sizeof(obj));
-  o -> x = playerx;
-  o -> y = playery;
+  o -> x = gPlayerx;
+  o -> y = gPlayery;
   o -> id = inv[id];
 
   if (addobj(o)==-1) {
@@ -354,16 +358,16 @@ bool mechanics(int key) {
   }
 
   switch (key) {
-    case cont[EXIT]:return 0;
-    case cont[INV]:inventory();return 1;
+    case gCont[EXIT]:return 0;
+    case gCont[INV]:inventory();return 1;
 
-    case cont[UP]:movep(playerx+0,playery-1);return 1;
-    case cont[DOWN]:movep(playerx+0,playery+1);return 1;
-    case cont[LEFT]:movep(playerx-1,playery+0);return 1;
-    case cont[RIGHT]:movep(playerx+1,playery+0);return 1;
-    case cont[RESTART]:restart(1);return 1;
-    case cont[DROP]:drop();return 1;
-    case cont[DEBUG]:return 0;
+    case gCont[UP]:movep(gPlayerx+0,gPlayery-1);return 1;
+    case gCont[DOWN]:movep(gPlayerx+0,gPlayery+1);return 1;
+    case gCont[LEFT]:movep(gPlayerx-1,gPlayery+0);return 1;
+    case gCont[RIGHT]:movep(gPlayerx+1,gPlayery+0);return 1;
+    case gCont[RESTART]:restart(1);return 1;
+    case gCont[DROP]:drop();return 1;
+    case gCont[DEBUG]:return 0;
   }
 
   msg("Unrecognized command.");
@@ -378,7 +382,7 @@ void game() {
   noecho();
   curs_set(0);
   move(0,0);
-  printw("Text only game. [by M.Z.] -more- \nVerson : %s",verson);
+  printw("Text only game. [by M.Z.] -more- \ngVerson : %s",gVerson);
   refresh();
   getch();
   clear();
@@ -390,7 +394,7 @@ void game() {
     key = getch();
     running = mechanics(key);
     //cleanln(2);
-    //mvprintw(1,0,"%d",getRoomId(playerx,playery));
+    //mvprintw(1,0,"%d",getRoomId(gPlayerx,gPlayery));
     refresh();
   }
 
@@ -406,16 +410,24 @@ void fillroom(int room) { //fill a room room:roomid
   int index = room - 1;
   //mvprintw(0,0,"%d",room);
   if (room==0) { //if it is a passing
-    rendermap[playery][playerx] = map[playery][playerx]; //the player pos
+    rendermap[gPlayery][gPlayerx] = map[gPlayery][gPlayerx]; //the player pos
 
-    if (map[playery-1][playerx] == '#' || map[playery-1][playerx] == '+' || map[playery-1][playerx] == '.') {rendermap[playery-1][playerx] = map[playery-1][playerx];} //all agationt points
-    if (map[playery+1][playerx] == '#' || map[playery][playerx+1] == '+'|| map[playery][playerx+1] == '.') {rendermap[playery][playerx+1] = map[playery][playerx+1];}
-    if (map[playery][playerx+1] == '#' || map[playery][playerx+1] == '+' || map[playery][playerx+1] == '.') {rendermap[playery][playerx+1] = map[playery][playerx+1];}
-    if (map[playery][playerx-1] == '#' || map[playery][playerx-1] == '+' || map[playery][playerx-1] == '.') {rendermap[playery][playerx-1] = map[playery][playerx-1];}
+    if (map[gPlayery-1][gPlayerx] == '#' || map[gPlayery-1][gPlayerx] == '+' || map[gPlayery-1][gPlayerx] == '.') {
+      rendermap[gPlayery-1][gPlayerx] = map[gPlayery-1][gPlayerx];
+    } //all agationt points
+    if (map[gPlayery+1][gPlayerx] == '#' || map[gPlayery][gPlayerx+1] == '+'|| map[gPlayery][gPlayerx+1] == '.') {
+      rendermap[gPlayery][gPlayerx+1] = map[gPlayery][gPlayerx+1];
+    }
+    if (map[gPlayery][gPlayerx+1] == '#' || map[gPlayery][gPlayerx+1] == '+' || map[gPlayery][gPlayerx+1] == '.') {
+      rendermap[gPlayery][gPlayerx+1] = map[gPlayery][gPlayerx+1];
+    }
+    if (map[gPlayery][gPlayerx-1] == '#' || map[gPlayery][gPlayerx-1] == '+' || map[gPlayery][gPlayerx-1] == '.') {
+      rendermap[gPlayery][gPlayerx-1] = map[gPlayery][gPlayerx-1];
+    }
 
-    cleanln(playery+1); //redraw it all
-    cleanln(playery+2);
-    cleanln(playery);
+    cleanln(gPlayery+1); //redraw it all
+    cleanln(gPlayery+2);
+    cleanln(gPlayery);
 
     return;
   }
@@ -428,7 +440,6 @@ void fillroom(int room) { //fill a room room:roomid
 }
 
 void renderline(char* line,int y) {//render a line line: text y: pos on screan
-
   move(y,0);
   for (int x = 0;x<MAPX;x++) {
     mvaddch(y,x,line[x]);
