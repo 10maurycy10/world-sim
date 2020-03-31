6cbbed2c40f2cf98143d9f8f63f9e20ea3ebff35
10maurycy10 <10maurycy10@users.noreply.github.com>
2020-03-30 20:11:54 -0800

    clean up

--- comp.bat
+++
@@ -1,2 +0,0 @@
-
-gcc scr\main.c -o main.exe -lcurses2 scr/SDL2.lib -Wall -Wextra -pedantic -Dnot_FULL_SCREEN
--- p.bat
+++
@@ -1,3 +0,0 @@
-echo off
-gcc scr\main.c -o pb.exe -lcurses2 scr/SDL2.lib -Wall -Wextra -pedantic -DP -s -O3 && echo BUILD OK
-strip pb.exe && echo STIP OK
